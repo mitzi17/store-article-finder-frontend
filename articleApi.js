301f1b37d41768b7e0f223584a53a672725d7080
Mitzi17 <m_estrada17@live.com>
2021-04-29 16:30:02 -0700

    create file, insert fetchArticles() from index.js and define createArticle()

---
+++ articleApi.js
@@ -0,0 +1,43 @@
+class articleApi {
+    static baseURL = 'http://localhost:3000/articles'
+
+    static fetchArticles() {
+        fetch(this.baseURL)
+        .then(response => response.json())
+        .then(dataArray => dataArray["data"].forEach(article => { 
+        const newArticle = new Article({id: article.id, ...article.attributes})
+        newArticle.attachToDom()
+        })
+    )}
+
+    static createArticle() {
+        const formData = {
+            name: nameField.value,
+            number: numberField.value,
+            price: priceField.value,
+            size: sizeField.value,
+            location_id: locationDropdown.value,
+            category_id: categoryDropdown.value
+        }
+        debugger
+    
+        const configObj = {
+            method: 'POST', 
+            headers: { 
+                "Content-Type": "application/json", 
+                Accept: "application/json"
+            },
+            body: JSON.stringify(formData) 
+        }
+    
+        fetch(this.baseURL, configObj)
+            .then(response => response.json())
+            .then(data => {
+                const article = data.data
+                const newArticle = new Article({id: article.id, ...article.attributes})
+                newArticle.attachToDom()
+            })
+    }
+
+        
+}
