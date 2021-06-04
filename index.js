4df9016b3f18536ce239d1614224df2fedec47aa
Mitzi17 <m_estrada17@live.com>
2021-06-04 12:36:24 -0700

    create sort button and add eventlistner to button

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const sizeField = document.getElementById("grid-size")
 const locationDropdown = document.getElementById("grid-location")
 const categoryField = document.getElementById("grid-category")
 
+const locationContainer = document.getElementById("location-container")
+const sortBtn = document.createElement('button')
 
 
 
@@ -30,7 +32,20 @@ articleApi.fetchArticles()
 locationApi.fetchLocations()
 
 
+function sortButton() {
+   
+    sortBtn.innerText = "Sort"
+    sortBtn.setAttribute("class", "ml-5 tems-center justify-center rounded-md border border-gray-300")
+    locationContainer.append(sortBtn)
+}
 
+sortButton()
 
+sortBtn.addEventListener("click", handleSortBtn)
 
+function handleSortBtn(e) {
+    e.preventDefault()
+    Article.sortArticles()
+    
+}
 
